6add8815ab919dcf8d1ed71d728e1a0e1d636162
peche56 <c.filleul56@gmail.com>
2017-03-13 15:12:36 +0100

    first commit

---
+++ anim.js
@@ -0,0 +1,58 @@
+jQuery(document).ready(function() {
+
+    $img = $('img');
+    i = 0;
+    nombreImg = $img.length - 1;
+    $imgVisible = $img.eq(i);
+    var $prev = $('#prev');
+    var $next = $('#next');
+    // $( "img" ).not("imgVisible").hide();
+
+
+
+
+
+
+
+
+    $next.click(function() {
+
+        i++;
+        $img.css('display', 'none');
+        $imgVisible = $img.eq(i);
+        $imgVisible.css('display', 'block');
+
+        if (i > nombreImg) {
+            i = 0;
+        }
+    });
+
+    $prev.click(function() {
+
+        i--;
+        $img.css('display', 'none');
+        $imgVisible = $img.eq(i);
+        $imgVisible.css('display', 'block');
+
+        if (i < 0) {
+            i = nombreImg;
+        }
+
+    });
+
+    var autorun = setInterval(function() {
+
+        if (i > nombreImg) {
+          i = 0;
+        } else {
+            i++;
+        }
+
+        $img.css('display', 'none');
+        $imgVisible = $img.eq(i);
+        $imgVisible.css('display', 'block');
+
+
+    }, 3000);
+
+});
